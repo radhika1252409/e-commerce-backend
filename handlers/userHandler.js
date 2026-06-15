468010d78adcb25d73ce3c62cd4e5a97b03b4944
Radhika <radhika_pulupula@srmap.edu.in>
2026-06-15 16:08:46 +0530

    Create userHandler.js

---
+++ handlers/userHandler.js
@@ -0,0 +1,25 @@
+const userService = require("../services/userService");
+
+exports.createUser = async (req, res) => {
+    const user = await userService.createUser(req.body);
+    res.json(user);
+};
+
+exports.getUsers = async (req, res) => {
+    const users = await userService.getUsers();
+    res.json(users);
+};
+
+exports.updateUser = async (req, res) => {
+    const user = await userService.updateUser(
+        req.params.id,
+        req.body
+    );
+
+    res.json(user);
+};
+
+exports.deleteUser = async (req, res) => {
+    await userService.deleteUser(req.params.id);
+    res.json({ message: "User Deleted" });
+};
